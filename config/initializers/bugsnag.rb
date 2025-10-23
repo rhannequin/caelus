4246ba807ac8821ce8bf63a486d3de52ebcf15d0
Rémy Hannequin <remy@thoughtbot.com>
2025-10-23 22:34:52 +0200

    Proper documentation and better setup

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-Bugsnag.configure do |config|
-  config.api_key = ENV["BUGSNAG_API_KEY"]
+if ENV["BUGSNAG_API_KEY"].present?
+  Bugsnag.configure do |config|
+    config.api_key = ENV["BUGSNAG_API_KEY"]
 
-  config.release_stage = Rails.env
+    config.release_stage = Rails.env
 
-  config.notify_release_stages = ["production"]
+    config.notify_release_stages = ["production"]
+  end
 end
