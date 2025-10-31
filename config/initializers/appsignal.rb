398fd43dbbf7f8d1aae15918d0eebfc4a72cc892
Rémy Hannequin <remy@thoughtbot.com>
2025-10-31 15:35:26 +0100

    Improve Appsignal setup

--- config/initializers/appsignal.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-if ENV["APPSIGNAL_PUSH_API_KEY"].present?
-  Appsignal.configure do |config|
-    config.activate_if_environment(:production)
-    config.push_api_key = ENV["APPSIGNAL_PUSH_API_KEY"]
-  end
-end
