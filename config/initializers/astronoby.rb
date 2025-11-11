e1d71ad8eb1919bc347c22e93adda6a52240a248
Rémy Hannequin <remy@thoughtbot.com>
2025-11-11 23:14:47 +0100

    Add an optimised ephemeris for every year

--- config/initializers/astronoby.rb
+++ config/initializers/astronoby.rb
@@ -12,7 +12,29 @@ class SPK
   end
 
   def inpop19a
-    @inpop19a ||=
-      Astronoby::Ephem.load("lib/astronoby/spk/inpop19a_2024_2026.bsp")
+    @inpop19a ||= Astronoby::Ephem.load("lib/astronoby/spk/inpop19a.bsp")
+  end
+
+  def self.for_time(time)
+    instance.for_time(time)
+  end
+
+  def for_time(time)
+    year = time.utc.year
+    var_name = "@inpop19a_#{year}"
+
+    instance_variable_get(var_name) || instance_variable_set(
+      var_name,
+      (
+        previous_year = year - 1
+        next_year = year + 1
+        file_path = "lib/astronoby/spk/inpop19a_#{previous_year}_#{next_year}.bsp"
+        if File.exist?(file_path)
+          Astronoby::Ephem.load(file_path)
+        else
+          inpop19a
+        end
+      )
+    )
   end
 end
