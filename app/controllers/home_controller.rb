723b31870c72faceb97cbf32a2c40d511d828b2d
Rémy Hannequin <remy.hannequin@gmail.com>
2026-06-05 23:13:29 +0200

    Remove unused cache system (#137)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,34 +4,19 @@ class HomeController < ApplicationController
   MAXIMUM_DEEP_SKY_OBJECTS = 10
 
   def index
-    @planets = Rails.cache.fetch(
-      "planets/#{observer_daily_cache_key}",
-      expires_at: observer_end_of_day
-    ) do
-      [
-        Mercury.new(observer: @observer, time: @time),
-        Venus.new(observer: @observer, time: @time),
-        Mars.new(observer: @observer, time: @time),
-        Jupiter.new(observer: @observer, time: @time),
-        Saturn.new(observer: @observer, time: @time),
-        Uranus.new(observer: @observer, time: @time),
-        Neptune.new(observer: @observer, time: @time)
-      ]
-    end
+    @planets = [
+      Mercury.new(observer: @observer, time: @time),
+      Venus.new(observer: @observer, time: @time),
+      Mars.new(observer: @observer, time: @time),
+      Jupiter.new(observer: @observer, time: @time),
+      Saturn.new(observer: @observer, time: @time),
+      Uranus.new(observer: @observer, time: @time),
+      Neptune.new(observer: @observer, time: @time)
+    ]
 
-    @sun = Rails.cache.fetch(
-      "sun/#{observer_daily_cache_key}",
-      expires_in: 1.hour
-    ) do
-      Sun.new(observer: @observer, time: @time)
-    end
+    @sun = Sun.new(observer: @observer, time: @time)
 
-    @moon = Rails.cache.fetch(
-      "moon/#{observer_daily_cache_key}",
-      expires_in: 1.hour
-    ) do
-      Moon.new(observer: @observer, time: @time)
-    end
+    @moon = Moon.new(observer: @observer, time: @time)
 
     @twilight_events = Rails.cache.fetch(
       "twilight_events/#{observer_daily_cache_key}",
@@ -56,16 +41,11 @@ class HomeController < ApplicationController
       ).event_on(@time.to_date + 1)
     end
 
-    @messier_object_positions = Rails.cache.fetch(
-      "messier_object_positions/#{observer_daily_cache_key}",
-      expires_at: observer_end_of_day
-    ) do
-      MessierCatalog
-        .all
-        .sort_by(&:magnitude)
-        .first(MAXIMUM_DEEP_SKY_OBJECTS)
-        .map { |obj| obj.at(@time, observer: @observer, use_ephem: true) }
-    end
+    @messier_object_positions = MessierCatalog
+      .all
+      .sort_by(&:magnitude)
+      .first(MAXIMUM_DEEP_SKY_OBJECTS)
+      .map { |obj| obj.at(@time, observer: @observer, use_ephem: true) }
 
     track_page_view("home")
   end
--- app/controllers/sun_controller.rb
+++ app/controllers/sun_controller.rb
@@ -12,17 +12,12 @@ class SunController < ApplicationController
   def show
     @sun = Sun.new(observer: @observer, time: @time)
     @yesterday_sun = Sun.new(observer: @observer, time: @time - 1.day)
-    @yearly_elevation = Rails.cache.fetch(
-      "sun_elevation/#{observer_yearly_cache_key}",
-      expires_at: observer_end_of_year
-    ) do
-      YearlyElevation.new(
-        year: @time.year,
-        body: Sun,
-        observer: @observer,
-        samples: Date.gregorian_leap?(@time.year) ? 366 : 365
-      )
-    end
+    @yearly_elevation = YearlyElevation.new(
+      year: @time.year,
+      body: Sun,
+      observer: @observer,
+      samples: Date.gregorian_leap?(@time.year) ? 366 : 365
+    )
     @twilight_event = twilight_calculator.event_on(
       @time.to_date,
       utc_offset: @observer.time_zone.formatted_offset
