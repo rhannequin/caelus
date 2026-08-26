404dc528b941a3a5a114eaa14918cc31d12ca878
Rémy Hannequin <remy@thoughtbot.com>
2026-08-26 22:20:27 +0200

    Show the published position angle of each contact

--- app/helpers/lunar_eclipse_helper.rb
+++ app/helpers/lunar_eclipse_helper.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
 module LunarEclipseHelper
+  Contact = Struct.new(:key, :time, :position_angle)
+
   def lunar_eclipse_contacts(lunar_eclipse)
+    penumbral = lunar_eclipse.penumbral
     umbral = lunar_eclipse.partial
     totality = lunar_eclipse.total
 
     [
-      [:p1, lunar_eclipse.penumbral.starting_instant],
-      ([:u1, umbral.starting_instant] if umbral),
-      ([:u2, totality.starting_instant] if totality),
-      [:greatest, lunar_eclipse.instant],
-      ([:u3, totality.ending_instant] if totality),
-      ([:u4, umbral.ending_instant] if umbral),
-      [:p4, lunar_eclipse.penumbral.ending_instant]
-    ].compact.map { |key, instant| [key, instant.to_time] }
+      [:p1, penumbral.starting_instant, penumbral.starting_geometry],
+      ([:u1, umbral.starting_instant, umbral.starting_geometry] if umbral),
+      ([:u2, totality.starting_instant, totality.starting_geometry] if totality),
+      [:greatest, lunar_eclipse.instant, lunar_eclipse.geometry],
+      ([:u3, totality.ending_instant, totality.ending_geometry] if totality),
+      ([:u4, umbral.ending_instant, umbral.ending_geometry] if umbral),
+      [:p4, penumbral.ending_instant, penumbral.ending_geometry]
+    ].compact.map do |key, instant, geometry|
+      Contact.new(key, instant.to_time, geometry.position_angle)
+    end
   end
 
   def lunar_eclipse_phases(lunar_eclipse)
